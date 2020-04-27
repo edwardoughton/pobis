5e9200ce903b630bf9455ed5617e64ecac2bc7b7
ej550 <edward.oughton@gmail.com>
2020-04-27 09:46:37 +0100

    Update strategies

--- scripts/options.py
+++ scripts/options.py
@@ -8,165 +8,57 @@ Written by Ed Oughton
 
 January 2020
 
-#strategy is defined based on generation_core_backhaul_sharing_subsidy_spectrum_tax
+#strategy is defined based on generation_core_backhaul_sharing_subsidy_spectrum_tax_integration
 
 """
 OPTIONS = {
     'technology_options': [
         {
-            'scenario': 'S1_25_10_5',
-            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '3G_epc_microwave_baseline_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S2_200_50_25',
-            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '3G_epc_fiber_baseline_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S3_400_100_50',
-            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S1_25_10_5',
-            'strategy': '4G_epc_fiber_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '4G_epc_fiber_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '4G_epc_fiber_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_sa_fiber_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_sa_fiber_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_sa_fiber_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_fiber_baseline_baseline_baseline_baseline_baseline',
         },
     ],
     'business_model_options': [
         {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_passive_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_passive_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_passive_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_passive_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_active_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_active_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_active_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_active_baseline_baseline_baseline_baseline',
         },
     ],
     'policy_options': [
         {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline_baseline',
         },
         {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_low_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_low_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_low_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_high_baseline',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_high_baseline',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_high_baseline',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_low',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_low',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_low',
-        },
-        {
-            'scenario': 'S1_25_10_5',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_high',
-        },
-        {
-            'scenario': 'S2_200_50_25',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_high',
-        },
-        {
-            'scenario': 'S3_400_100_50',
-            'strategy': '5G_nsa_microwave_baseline_baseline_baseline_high',
+            'scenario': 'S1_2_2_2',
+            'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline_integration',
         },
     ]
 }
 
 
 COUNTRY_PARAMETERS = {
-    'ALB': {
+    'CIV': {
         'luminosity': {
             'high': 5,
             'medium': 1,
@@ -179,6 +71,16 @@ COUNTRY_PARAMETERS = {
         'networks': 3,
         'proportion_of_sites': 50,
         'frequencies': {
+            '3G': [
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 2100,
+                    'bandwidth': 10,
+                },
+            ],
             '4G': [
                 {
                     'frequency': 800,
@@ -189,16 +91,6 @@ COUNTRY_PARAMETERS = {
                     'bandwidth': 10,
                 },
             ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
         },
         'financials': {
             'profit_margin': 10,
@@ -224,6 +116,16 @@ COUNTRY_PARAMETERS = {
         'networks': 3,
         'proportion_of_sites': 50,
         'frequencies': {
+            '3G': [
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 2100,
+                    'bandwidth': 10,
+                },
+            ],
             '4G': [
                 {
                     'frequency': 800,
@@ -234,16 +136,6 @@ COUNTRY_PARAMETERS = {
                     'bandwidth': 10,
                 },
             ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
         },
         'financials': {
             'profit_margin': 10,
@@ -256,7 +148,7 @@ COUNTRY_PARAMETERS = {
             'tax_high': 40,
             },
         },
-    'MEX': {
+    'MLI': {
         'luminosity': {
             'high': 5,
             'medium': 1,
@@ -269,6 +161,16 @@ COUNTRY_PARAMETERS = {
         'networks': 3,
         'proportion_of_sites': 50,
         'frequencies': {
+            '3G': [
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 2100,
+                    'bandwidth': 10,
+                },
+            ],
             '4G': [
                 {
                     'frequency': 800,
@@ -279,156 +181,6 @@ COUNTRY_PARAMETERS = {
                     'bandwidth': 10,
                 },
             ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
-        },
-        'financials': {
-            'profit_margin': 10,
-            'subsidy_low': 10,
-            'subsidy_high': 40,
-            'spectrum_coverage_baseline_usd_mhz_pop': 0.3,
-            'spectrum_capacity_baseline_usd_mhz_pop': 0.1,
-            'tax_low': 10,
-            'tax_baseline': 25,
-            'tax_high': 40,
-            },
-        },
-    'MWI': {
-        'luminosity': {
-            'high': 5,
-            'medium': 1,
-        },
-        'arpu': {
-            'high': 10,
-            'medium': 3,
-            'low': 2,
-        },
-        # Access Comm, Airtel, TNM
-        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
-        'networks': 2,
-        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
-        'proportion_of_sites': 50,
-        'frequencies': {
-            '4G': [
-                {
-                    'frequency': 800,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 1800,
-                    'bandwidth': 10,
-                },
-            ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
-        },
-        'financials': {
-            'profit_margin': 10,
-            'subsidy_low': 10,
-            'subsidy_high': 40,
-            'spectrum_coverage_baseline_usd_mhz_pop': 0.3,
-            'spectrum_capacity_baseline_usd_mhz_pop': 0.1,
-            'tax_low': 10,
-            'tax_baseline': 25,
-            'tax_high': 40,
-            },
-        },
-    'PAK': {
-        'luminosity': {
-            'high': 5,
-            'medium': 1,
-        },
-        'arpu': {
-            'high': 5,
-            'medium': 2,
-            'low': 1,
-        },
-        # also GSMA, 2019 (same report as above)
-        # smartphone pen was 10% in 2017, so assume 15% in 2020
-        'smartphone_pen': 0.5,
-        # Access Comm, Airtel, TNM
-        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
-        'networks': 3,
-        # https://en.wikipedia.org/wiki/List_of_LTE_networks_in_Africa
-        'proportion_of_sites': 50,
-        'frequencies': {
-            '4G': [
-                {
-                    'frequency': 800,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 1800,
-                    'bandwidth': 10,
-                },
-            ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
-        },
-        'financials': {
-            'profit_margin': 10,
-            'subsidy_low': 10,
-            'subsidy_high': 40,
-            'spectrum_coverage_baseline_usd_mhz_pop': 0.03,
-            'spectrum_capacity_baseline_usd_mhz_pop': 0.01,
-            'tax_low': 10,
-            'tax_baseline': 25,
-            'tax_high': 40,
-            },
-        },
-    'PER': {
-        'luminosity': {
-            'high': 5,
-            'medium': 1,
-        },
-        'arpu': {
-            'high': 15,
-            'medium': 12,
-            'low': 5,
-        },
-        'networks': 3,
-        'proportion_of_sites': 50,
-        'frequencies': {
-            '4G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 30,
-                },
-                {
-                    'frequency': 2600,
-                    'bandwidth': 20,
-                },
-            ],
-            '5G': [
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
         },
         'financials': {
             'profit_margin': 10,
@@ -455,6 +207,16 @@ COUNTRY_PARAMETERS = {
         'networks': 3,
         'proportion_of_sites': 50,
         'frequencies': {
+            '3G': [
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 2100,
+                    'bandwidth': 10,
+                },
+            ],
             '4G': [
                 {
                     'frequency': 800,
@@ -465,16 +227,51 @@ COUNTRY_PARAMETERS = {
                     'bandwidth': 10,
                 },
             ],
-            '5G': [
+        },
+        'financials': {
+            'profit_margin': 10,
+            'subsidy_low': 10,
+            'subsidy_high': 40,
+            'spectrum_coverage_baseline_usd_mhz_pop': 0.3,
+            'spectrum_capacity_baseline_usd_mhz_pop': 0.1,
+            'tax_low': 10,
+            'tax_baseline': 25,
+            'tax_high': 40,
+            },
+        },
+    'TZA': {
+        'luminosity': {
+            'high': 5,
+            'medium': 1,
+        },
+        'arpu': {
+            'high': 10,
+            'medium': 3,
+            'low': 2,
+        },
+        'networks': 2,
+        'proportion_of_sites': 50,
+        'frequencies': {
+            '3G': [
                 {
-                    'frequency': 700,
+                    'frequency': 1800,
                     'bandwidth': 10,
                 },
                 {
-                    'frequency': 3500,
-                    'bandwidth': 50,
+                    'frequency': 2100,
+                    'bandwidth': 10,
                 },
-            ]
+            ],
+            '4G': [
+                {
+                    'frequency': 800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+            ],
         },
         'financials': {
             'profit_margin': 10,
@@ -500,26 +297,26 @@ COUNTRY_PARAMETERS = {
         'networks': 2,
         'proportion_of_sites': 50,
         'frequencies': {
+            '3G': [
+                {
+                    'frequency': 1800,
+                    'bandwidth': 10,
+                },
+                {
+                    'frequency': 2100,
+                    'bandwidth': 10,
+                },
+            ],
             '4G': [
                 {
                     'frequency': 800,
                     'bandwidth': 10,
                 },
                 {
-                    'frequency': 2600,
+                    'frequency': 1800,
                     'bandwidth': 10,
                 },
             ],
-            '5G': [
-                {
-                    'frequency': 700,
-                    'bandwidth': 10,
-                },
-                {
-                    'frequency': 3500,
-                    'bandwidth': 50,
-                },
-            ]
         },
         'financials': {
             'profit_margin': 10,
