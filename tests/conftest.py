a6f5cd632b53db2f716bfcf97691a16d4f3b7ad1
ej550 <edward.oughton@gmail.com>
2020-07-09 14:33:34 +0100

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,250 @@
+from pytest import fixture
+
+
+@fixture(scope='function')
+def setup_region():
+    return [{
+    'GID_0': 'MWI',
+    'GID_id': 'MWI.1.1.1_1',
+    'mean_luminosity_km2': 26.736407691655717,
+    'population': 10000,
+    'area_km2': 2,
+    'population_km2': 5000,
+    'decile': 100,
+    'geotype': 'urban',
+    'demand_mbps_km2': 5000,
+    'integration': 'baseline'
+    }]
+
+
+@fixture(scope='function')
+def setup_region_rural():
+    return [{
+    'GID_0': 'MWI',
+    'GID_id': 'MWI.1.1.1_1',
+    'mean_luminosity_km2': 26.736407691655717,
+    'population': 10000,
+    'area_km2': 2,
+    'population_km2': 5000,
+    'decile': 100,
+    'geotype': 'rural'
+    }]
+
+
+@fixture(scope='function')
+def setup_option():
+    return { #generation_core_backhaul_sharing_networks_spectrum_tax_integration
+        'scenario': 'S1_50_50_50',
+        'strategy': '4G_epc_microwave_baseline_baseline_baseline_baseline_baseline_baseline'
+    }
+
+
+@fixture(scope='function')
+def setup_option_high():
+    return {
+        'scenario': 'S1_50_50_50',
+        'strategy': '4G_epc_microwave_baseline_baseline_high_high_high'
+    }
+
+
+@fixture(scope='function')
+def setup_global_parameters():
+    return {
+        'overbooking_factor': 100,
+        'return_period': 2,
+        'discount_rate': 5,
+        'opex_percentage_of_capex': 10,
+        'sectorization': 3,
+        'confidence': [1, 10, 50],
+        'cots_processing_split_urban': 2,
+        'cots_processing_split_suburban': 4,
+        'cots_processing_split_rural': 16,
+        'io_n2_n3_split': 7,
+        'low_latency_switch_split': 7,
+        'rack_split': 7,
+        'cloud_power_supply_converter_split': 7,
+        'software_split': 7,
+        'cloud_backhaul_split': 7,
+        'regional_integration_factor': 10
+    }
+
+
+@fixture(scope='function')
+def setup_country_parameters():
+    return {
+        'luminosity': {
+            'high': 5,
+            'medium': 1,
+        },
+        'arpu': {
+            'high': 15,
+            'medium': 5,
+            'low': 2,
+        },
+        'networks': {
+            'baseline_urban': 2,
+            'baseline_suburban': 2,
+            'baseline_rural': 2,
+            # 'srn_urban': 2,
+            # 'srn_suburban': 2,
+            # 'srn_rural': 1,
+        },
+        'frequencies': {
+            '4G': [
+                {
+                    'frequency': 800,
+                    'bandwidth': '2x10',
+                },
+                {
+                    'frequency': 1800,
+                    'bandwidth': '2x10',
+                },
+            ],
+            '5G': [
+                {
+                    'frequency': 700,
+                    'bandwidth': '2x10',
+                },
+                {
+                    'frequency': 3500,
+                    'bandwidth': '1x50',
+                },
+            ]
+        },
+        'financials': {
+            'wacc': 15,
+            'profit_margin': 20,
+            'spectrum_coverage_baseline_usd_mhz_pop': 1,
+            'spectrum_capacity_baseline_usd_mhz_pop': 1,
+            'spectrum_cost_low': 50,
+            'spectrum_cost_high': 50,
+            'tax_low': 10,
+            'tax_baseline': 25,
+            'tax_high': 40,
+            'acquisition_per_subscriber': 10,
+            },
+        }
+
+
+@fixture(scope='function')
+def setup_timesteps():
+    return [
+        2020,
+        # 2021,
+        # 2022,
+        # 2023,
+        # 2024,
+        # 2025,
+        # 2026,
+        # 2027,
+        # 2028,
+        # 2029,
+        # 2030
+    ]
+
+
+@fixture(scope='function')
+def setup_penetration_lut():
+    return {
+        2020: 50,
+        # 2021: 75,
+    }
+
+
+@fixture(scope='function')
+def setup_costs():
+    return {
+        #all costs in $USD
+        'single_sector_antenna': 1500,
+        'single_remote_radio_unit': 4000,
+        'io_fronthaul': 1500,
+        'processing': 1500,
+        'io_s1_x2': 1500,
+        'control_unit': 1500,
+        'cooling_fans': 250,
+        'distributed_power_supply_converter': 250,
+        'power_generator_battery_system': 5000,
+        'bbu_cabinet': 500,
+        'fiber_fronthaul_urban_m': 10,
+        'fiber_fronthaul_suburban_m': 5,
+        'fiber_fronthaul_rural_m': 2,
+        'cots_processing': 500,
+        'io_n2_n3': 1500,
+        'low_latency_switch': 500,
+        'rack': 500,
+        'cloud_power_supply_converter': 1000,
+        'software': 50,
+        'tower': 10000,
+        'civil_materials': 5000,
+        'transportation': 5000,
+        'installation': 5000,
+        'site_rental_urban': 9600,
+        'site_rental_suburban': 4000,
+        'site_rental_rural': 2000,
+        'router': 2000,
+        'microwave_small': 10000,
+        'microwave_medium': 20000,
+        'microwave_large': 40000,
+        'fiber_urban_m': 10,
+        'fiber_suburban_m': 5,
+        'fiber_rural_m': 2,
+        'core_node_epc': 100000,
+        'core_node_nsa': 150000,
+        'core_node_sa': 200000,
+        'core_edge': 20,
+        'regional_node_epc': 100000,
+        'regional_node_nsa': 150000,
+        'regional_node_sa': 200000,
+        'regional_edge': 10,
+        'regional_node_lower_epc': 10000,
+        'regional_node_lower_nsa': 10000,
+        'regional_node_lower_sa': 10000,
+        'per_site_spectrum_acquisition_cost': 1000,
+        'per_site_administration_cost': 100,
+    }
+
+
+@fixture(scope='function')
+def setup_lookup():
+    return {
+        ('urban', 'macro', '800', '4G', '50'): [
+            (0.01, 1),
+            (0.02, 2),
+            (0.05, 5),
+            (0.15, 15),
+            (2, 100)
+        ],
+        ('urban', 'macro', '1800', '4G', '50'): [
+            (0.01, 5),
+            (0.02, 10),
+            (0.05, 20),
+            (0.15, 40),
+            (2, 1000)
+        ],
+    }
+
+
+@fixture(scope='function')
+def setup_ci():
+    return 50
+
+@fixture(scope='function')
+def setup_core_lut():
+    return {
+        'core_edge': {
+            'MWI.1.1.1_1_new': 1000,
+            'MWI.1.1.1_1_existing': 1000
+        },
+        'core_node': {
+            'MWI.1.1.1_1_new': 2,
+            'MWI.1.1.1_1_existing': 2
+        },
+        'regional_edge': {
+            'MWI.1.1.1_1_new': 1000,
+            'MWI.1.1.1_1_existing': 1000
+        },
+        'regional_node': {
+            'MWI.1.1.1_1_new': 2,
+            'MWI.1.1.1_1_existing': 2
+        },
+    }
---
+++ tests/test_supply.py
@@ -0,0 +1,244 @@
+import pytest
+from podis.demand import estimate_demand
+from podis.supply import (estimate_supply, find_site_density,
+    estimate_site_upgrades, estimate_backhaul_upgrades)
+
+
+def test_find_site_density(
+    setup_region,
+    setup_option,
+    setup_global_parameters,
+    setup_country_parameters,
+    setup_timesteps,
+    setup_penetration_lut,
+    setup_costs,
+    setup_lookup,
+    setup_ci
+    ):
+
+    #test demand being larger than max capacity
+    answer = find_site_density(
+        {'demand_mbps_km2': 100000,
+        'geotype': 'urban'},
+        setup_option, #4G using 800 + 1800
+        setup_country_parameters,
+        setup_lookup,
+        setup_ci
+    )
+
+    assert answer == 2
+
+    answer = find_site_density(
+        {'demand_mbps_km2': 0.005,
+        'geotype': 'urban'},
+        setup_option,
+        setup_country_parameters,
+        setup_lookup,
+        setup_ci
+    )
+
+    assert answer == 0.01
+
+    answer = find_site_density(
+        {'demand_mbps_km2': 250,
+        'geotype': 'urban'},
+        setup_option,
+        setup_country_parameters,
+        setup_lookup,
+        setup_ci
+    )
+
+    assert answer == 0.05
+
+    answer = find_site_density(
+        {'demand_mbps_km2': 120,
+        'geotype': 'urban'},
+        setup_option,
+        setup_country_parameters,
+        setup_lookup,
+        setup_ci
+    )
+
+    assert answer == 0.02
+
+
+def test_estimate_site_upgrades(
+    setup_region,
+    setup_option,
+    setup_country_parameters,
+    ):
+
+    #total sites across all opterators
+    setup_region[0]['sites_estimated_total'] = 100
+    setup_region[0]['sites_4G'] = 0
+
+    #100 sites in total across two operators, hence 50 existing sites for this MNO
+    answer = estimate_site_upgrades(
+        setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline_baseline',
+        100, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 2}}
+    )
+
+    assert answer['existing_network_sites'] == 50
+    assert answer['new_sites'] == 50
+    assert answer['upgraded_sites'] == 50
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 200
+    setup_region[0]['sites_4G'] = 50
+
+    #200 sites in total across two operators, hence 100 existing sites for this MNO
+    #100 sites required, hence no new sites or no new upgrades
+    answer = estimate_site_upgrades(setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+        100, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 2}}
+    )
+
+    assert answer['existing_network_sites'] == 100
+    assert answer['new_sites'] == 0
+    assert answer['upgraded_sites'] == 75
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 0
+    setup_region[0]['sites_4G'] = 0
+
+    #100 sites in total across two operators, hence 50 existing sites for this MNO
+    answer = estimate_site_upgrades(setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+        100, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 2}}
+    )
+
+    assert answer['existing_network_sites'] == 0
+    assert answer['new_sites'] == 100
+    assert answer['upgraded_sites'] == 0
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 100
+    setup_region[0]['sites_4G'] = 0
+
+    #100 sites in total across two operators, hence 50 existing sites for this MNO
+    answer = estimate_site_upgrades(setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+        100, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 10}}
+    )
+
+    assert answer['existing_network_sites'] == 10
+    assert answer['new_sites'] == 90
+    assert answer['upgraded_sites'] == 10
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 100
+    setup_region[0]['sites_4G'] = 50
+
+    #100 sites in total across two operators
+    #Hence 50 existing sites for this MNO (all techs, 2G-4G)
+    #However, 50 total are 4G, which means 25 for this MNO
+    #So we need to upgrade
+    answer = estimate_site_upgrades(setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+        100, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 2}}
+    )
+    #the MNO has 50 current sites for all techs (of which 25 are 4G)
+    assert answer['existing_network_sites'] == 50
+    #the MNO then builds 50 new sites
+    assert answer['new_sites'] == 50
+    #and upgrades 25 (totalling the 100 required)
+    assert answer['upgraded_sites'] == 25
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 100
+    setup_region[0]['sites_4G'] = 100
+
+    #100 sites in total across two operators, hence 50 existing sites for this MNO
+    answer = estimate_site_upgrades(setup_region[0],
+        '5g_nsa_microwave_baseline_baseline_baseline_baseline',
+        50, #100 sites required for this MNO
+        {'networks': {'baseline_urban': 2}}
+    )
+
+    assert answer['new_sites'] == 0
+    assert answer['upgraded_sites'] == 50
+
+
+def test_estimate_supply(
+    setup_region,
+    setup_lookup,
+    setup_option,
+    setup_global_parameters,
+    setup_country_parameters,
+    setup_costs,
+    setup_core_lut,
+    setup_ci
+    ):
+
+    #total sites across all operators
+    setup_region[0]['sites_estimated_total'] = 100
+    setup_region[0]['sites_4G'] = 0
+    setup_region[0]['backhaul_fiber'] = 0
+    setup_region[0]['backhaul_copper'] = 0
+    setup_region[0]['backhaul_microwave'] = 0
+    setup_region[0]['backhaul_satellite'] = 0
+
+    answer = estimate_supply('MWI',
+        setup_region,
+        setup_lookup,
+        setup_option,
+        setup_global_parameters,
+        setup_country_parameters,
+        setup_costs,
+        setup_core_lut,
+        setup_ci
+    )
+
+    assert round(answer[0]['site_density'], 1) == 0.9
+
+
+def test_estimate_backhaul_upgrades(
+    setup_region,
+    ):
+
+    setup_region[0]['new_sites'] = 50
+    setup_region[0]['upgraded_sites'] = 50
+
+    setup_region[0]['backhaul_fiber'] = 20
+    setup_region[0]['backhaul_copper'] = 20
+    setup_region[0]['backhaul_microwave'] = 50
+    setup_region[0]['backhaul_satellite'] = 10
+
+    answer = estimate_backhaul_upgrades(
+        setup_region[0],
+        '4G_epc_fiber_baseline_baseline_baseline_baseline',
+    )
+
+    assert answer['backhaul_new'] == 80
+
+    answer = estimate_backhaul_upgrades(
+        setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+    )
+
+    assert answer['backhaul_new'] == 30
+
+    setup_region[0]['backhaul_fiber'] = 100
+
+    answer = estimate_backhaul_upgrades(
+        setup_region[0],
+        '4G_epc_fiber_baseline_baseline_baseline_baseline',
+    )
+
+    assert answer['backhaul_new'] == 0
+
+    setup_region[0]['backhaul_fiber'] = 0
+    setup_region[0]['backhaul_microwave'] = 100
+
+    answer = estimate_backhaul_upgrades(
+        setup_region[0],
+        '4G_epc_microwave_baseline_baseline_baseline_baseline',
+    )
+
+    assert answer['backhaul_new'] == 0
