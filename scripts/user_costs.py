d9753b35f5fd128111da9d33ea4296b929970383
EdwardOughton <edward.oughton@gmail.com>
2022-07-18 09:56:03 -0400

    Revise user cost estimation

--- scripts/user_costs.py
+++ scripts/user_costs.py
@@ -37,71 +37,56 @@ def process_costs():
         All processed model results.
 
     """
-    #subset based on defined capacity
-    data = data[data['scenario'].str.contains(str(capacity))].reset_index()
+    data = []
 
-    # Add population density
-    data['population_density_km2'] = data['population'] / data['area_km2']
+    path = os.path.join(RESULTS, 'regional_results')
 
-    #relabel long strings
-    scenario = 'low_{}_{}_{}'.format(capacity, capacity, capacity)
-    data['scenario'] = data['scenario'].replace([scenario], 'Low')
-    scenario = 'baseline_{}_{}_{}'.format(capacity, capacity, capacity)
-    data['scenario'] = data['scenario'].replace([scenario], 'Baseline')
-    scenario = 'high_{}_{}_{}'.format(capacity, capacity, capacity)
-    data['scenario'] = data['scenario'].replace([scenario], 'High')
+    for filename in os.listdir(path):
 
-    data['strategy'] = data['strategy'].replace(['3G_epc_wireless_baseline_baseline_baseline_baseline_baseline'], '3G (W)')
-    data['strategy'] = data['strategy'].replace(['3G_epc_fiber_baseline_baseline_baseline_baseline_baseline'], '3G (FB)')
-    data['strategy'] = data['strategy'].replace(['4G_epc_wireless_baseline_baseline_baseline_baseline_baseline'], '4G (W)')
-    data['strategy'] = data['strategy'].replace(['4G_epc_fiber_baseline_baseline_baseline_baseline_baseline'], '4G (FB)')
+        # if not capacity in filename:
+        #     continue
 
-    data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=["private_cost_per_network_user"], how="all")
-    data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=["private_cost_per_smartphone_user"], how="all")
-    data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=["financial_cost_per_network_user"], how="all")
-    data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=["financial_cost_per_smartphone_user"], how="all")
+        if not 'regional_mno_cost_' in filename:
+            continue
 
-    data['private_cost_per_user'] = round(data['private_cost_per_network_user'])
-    data['financial_cost_per_user'] = round(data['financial_cost_per_network_user'])
+        filepath = os.path.join(path, filename)
 
-    bins = [-1, 20, 43, 69, 109, 171, 257, 367, 541, 1104, 111607] #-1,
-    labels = ['<20','20-43','43-69','69-109','109-171','171-257','257-367','367-541','541-1104','>1104']
+        sample = pd.read_csv(filepath)
 
-    data['decile'] = pd.cut(
-        data['population_density_km2'],
-        bins=bins,
-        labels=labels
-    )
+        # Add population density
+        sample['population_density_km2'] = sample['population'] / sample['area_km2']
 
-    return data
+        sample = sample.replace([np.inf, -np.inf], np.nan).dropna(subset=["private_cost_per_network_user"], how="all")
+        sample = sample.replace([np.inf, -np.inf], np.nan).dropna(subset=["private_cost_per_smartphone_user"], how="all")
+        sample = sample.replace([np.inf, -np.inf], np.nan).dropna(subset=["financial_cost_per_network_user"], how="all")
+        sample = sample.replace([np.inf, -np.inf], np.nan).dropna(subset=["financial_cost_per_smartphone_user"], how="all")
 
+        sample['private_cost_per_user'] = round(sample['private_cost_per_network_user'])
+        sample['financial_cost_per_user'] = round(sample['financial_cost_per_network_user'])
 
-def summarize_data(data, capacity):
-    """
-    Summarize the data by taking the mean costs across the
-    scenarios and strategies.
+        bins = [-1, 20, 43, 69, 109, 171, 257, 367, 541, 1104, 111607] #-1,
+        labels = ['<20','20-43','43-69','69-109','109-171','171-257','257-367','367-541','541-1104','>1104']
 
-    Parameters
-    ----------
-    data : pandas df
-        All model results.
-    capacity : int
-        The capacity we wish to process.
+        sample['decile'] = pd.cut(
+            sample['population_density_km2'],
+            bins=bins,
+            labels=labels
+        )
 
-    Returns
-    -------
-    data : pandas df
-        All processed model results.
+        sample = sample.to_dict('records')
 
-    """
+        data = data + sample
+
+    data = pd.DataFrame(data)
+
     #subset desired columns
     data = data[[
-        'scenario', 'strategy', 'decile', 'confidence',
+        'scenario', 'strategy', 'decile', 'confidence', 'input_cost',
         'private_cost_per_user', 'financial_cost_per_user',
         'government_cost_per_network_user'
     ]]
 
-    data = data.groupby(['scenario', 'strategy', 'confidence', 'decile'], as_index=False).agg(
+    data = data.groupby(['scenario', 'strategy', 'confidence', 'decile', 'input_cost'], as_index=False).agg(
             private_cost_per_user_mean = ('private_cost_per_user','mean'),
             government_cost_per_network_user_mean = ('government_cost_per_network_user','mean'),
             financial_cost_per_user_mean = ('financial_cost_per_user','mean'),
@@ -111,19 +96,19 @@ def summarize_data(data, capacity):
         )
 
     data.columns = [
-        'Scenario', 'Strategy', 'Confidence', 'Decile',
+        'Scenario', 'Strategy', 'Confidence', 'Decile', 'Input Cost',
         'private_mean_cpu',
         'govt_mean_cpu',
         'financial_mean_cpu'
-        # 'private_median_cpu',
-        # 'govt_median_cpu',
-        # 'financial_median_cpu',
     ]
 
-    return data
+    path = os.path.join(OUTPUT, 'decile_user_costs.csv')
+    data.to_csv(path, index=False)
+
+    return
 
 
-def process_all_regional_data(data):
+def process_all_regional_data():
     """
     Add any necessary variables and rename scenarios
     and strategies.
@@ -139,15 +124,16 @@ def process_all_regional_data(data):
         All processed model results.
 
     """
+    #Loading regional data by pop density geotype
+    filename = 'all_regional_data.csv'
+    path = os.path.join(DATA_INTERMEDIATE, filename)
+    data = pd.read_csv(path)
 
     data = data[[
         'GID_0','GID_id','GID_level',
         'population',
-        # 'pop_under_10_pop',
-        # 'pop_adults',
         'population_km2',
         'area_km2',
-        # 'pop_adults_km2'
     ]].copy()
 
     bins = [-1, 20, 43, 69, 109, 171, 257, 367, 541, 1104, 111607] #-1,
@@ -159,27 +145,9 @@ def process_all_regional_data(data):
         labels=labels
     )
 
-    return data
+    path = os.path.join(OUTPUT, 'decile_user_costs.csv')
+    costs = pd.read_csv(path)
 
-
-def get_costs(data, costs):
-    """
-    Combining the regional data and cost data across
-    the scenarios and strategies.
-
-    Parameters
-    ----------
-    data : pandas df
-        All regional data created in scripts/uba_prep.py.
-    costs : pandas df
-        All cost results.
-
-    Returns
-    -------
-    output : list of dicts
-        All combined data.
-
-    """
     costs = costs.to_dict('records')
 
     lookup = {}
@@ -187,6 +155,7 @@ def get_costs(data, costs):
     scenarios = set()
     strategies = set()
     cis = set()
+    input_costs = set()
 
     for item in costs:
 
@@ -194,8 +163,9 @@ def get_costs(data, costs):
         strategy = item['Strategy'].replace(' ', '')
         ci = item['Confidence']
         decile = item['Decile']
+        input_cost = item['Input Cost']
 
-        handle = '{}_{}_{}_{}'.format(scenario, strategy, ci, decile)
+        handle = '{}_{}_{}_{}_{}'.format(scenario, strategy, ci, decile, input_cost)
 
         lookup[handle] = {
             'Private Cost Per User ($USD)': item['private_mean_cpu'],
@@ -205,6 +175,7 @@ def get_costs(data, costs):
         scenarios.add(scenario)
         strategies.add(strategy)
         cis.add(ci)
+        input_costs.add(input_cost)
 
     output = []
 
@@ -213,38 +184,50 @@ def get_costs(data, costs):
     for scenario in list(scenarios):
         for strategy in list(strategies):
             for ci in list(cis):
-                for item in data:
+                for input_cost in input_costs:
+                    for item in data:
 
-                    handle = '{}_{}_{}_{}'.format(scenario, strategy, ci, item['decile'])
+                        handle = '{}_{}_{}_{}_{}'.format(
+                            scenario,
+                            strategy,
+                            ci,
+                            item['decile'],
+                            input_cost
+                        )
 
-                    private_cost_per_user = lookup[handle]['Private Cost Per User ($USD)']
-                    govt_cost_per_user = lookup[handle]['Government Cost Per User ($USD)']
-                    # financial_cost_per_user = lookup[handle]['Financial Cost Per User ($USD)']
+                        private_cost_per_user = lookup[handle]['Private Cost Per User ($USD)']
+                        govt_cost_per_user = lookup[handle]['Government Cost Per User ($USD)']
 
-                    output.append({
-                        'GID_id': item['GID_id'],
-                        'GID_level': item['GID_level'],
-                        'GID_0': item['GID_0'],
-                        'scenario': scenario,
-                        'strategy': strategy,
-                        'confidence': ci,
-                        'population': item['population'],
-                        'population_km2': item['population_km2'],
-                        'area_km2': item['area_km2'],
-                        'decile': item['decile'],
-                        'private_cost_per_user': private_cost_per_user,
-                        'govt_cost_per_user': govt_cost_per_user,
-                        'financial_cost_per_user': private_cost_per_user + govt_cost_per_user,
-                        'total_private_cost': item['population'] * private_cost_per_user,
-                        'total_government_cost': item['population'] * govt_cost_per_user,
-                        'total_financial_cost': (item['population'] *
-                        (private_cost_per_user + govt_cost_per_user)),
-                    })
+                        output.append({
+                            'GID_id': item['GID_id'],
+                            'GID_level': item['GID_level'],
+                            'GID_0': item['GID_0'],
+                            'scenario': scenario,
+                            'strategy': strategy,
+                            'confidence': ci,
+                            'input_cost': input_cost,
+                            'population': item['population'],
+                            'population_km2': item['population_km2'],
+                            'area_km2': item['area_km2'],
+                            'decile': item['decile'],
+                            'private_cost_per_user': private_cost_per_user,
+                            'govt_cost_per_user': govt_cost_per_user,
+                            'financial_cost_per_user': private_cost_per_user + govt_cost_per_user,
+                            'total_private_cost': item['population'] * private_cost_per_user,
+                            'total_government_cost': item['population'] * govt_cost_per_user,
+                            'total_financial_cost': (item['population'] *
+                            (private_cost_per_user + govt_cost_per_user)),
+                        })
+
+    output = pd.DataFrame(output)
+
+    path = os.path.join(OUTPUT, 'user_cost_estimates.csv')
+    output.to_csv(path, index=False)
 
     return output
 
 
-def processing_national_costs(regional_data, capacity):
+def processing_national_costs():
     """
     Processing national costs using regional data.
 
@@ -261,76 +244,76 @@ def processing_national_costs(regional_data, capacity):
         All combined data.
 
     """
-    national_costs = regional_data[[
-        'GID_0', 'scenario', 'strategy', 'confidence',
+    path = os.path.join(OUTPUT, 'user_cost_estimates.csv')
+    data = pd.read_csv(path)
+
+    national_costs = data[[
+        'GID_0', 'scenario', 'strategy', 'confidence', 'input_cost',
         'population', 'area_km2', 'total_private_cost',
         'total_government_cost', 'total_financial_cost'
     ]]
 
     national_costs = national_costs.groupby([
-        'GID_0', 'scenario', 'strategy', 'confidence'], as_index=True).sum().reset_index()
+        'GID_0', 'scenario', 'strategy', 'confidence', 'input_cost'], as_index=True).sum().reset_index()
 
     national_costs['total_private_cost'] = national_costs['total_private_cost'] / 1e9
     national_costs['total_government_cost'] = national_costs['total_government_cost'] / 1e9
     national_costs['total_financial_cost'] = national_costs['total_financial_cost'] / 1e9
 
-    national_costs.columns = [
-        'Country', 'Scenario', 'Strategy', 'Confidence',
-        'Population (>10 Years)', 'Area (Km2)', 'Private Cost ($Bn)',
-        'Government Cost ($Bn)', 'Financial Cost ($Bn)'
-    ]
+    path = os.path.join(OUTPUT, 'national_cost_estimates.csv')
+    national_costs.to_csv(path, index=False)
 
-    return national_costs
+    return
 
 
-def processing_decile_costs(regional_data):
+def processing_decile_costs():
     """
 
     """
-    total_costs = regional_data[[
-        'scenario', 'strategy', 'confidence', 'decile',
+    path = os.path.join(OUTPUT, 'user_cost_estimates.csv')
+    data = pd.read_csv(path)
+
+    decile_costs = data[[
+        'scenario', 'strategy', 'confidence', 'decile', 'input_cost',
         'population', 'area_km2', 'total_private_cost',
         'total_government_cost', 'total_financial_cost'
     ]]
 
-    total_costs = total_costs.groupby([
-        'scenario', 'strategy', 'confidence', 'decile',], as_index=True).sum().reset_index()
+    decile_costs = decile_costs.groupby([
+        'scenario', 'strategy', 'confidence', 'decile','input_cost'], as_index=True).sum().reset_index()
 
-    total_costs['total_private_cost'] = total_costs['total_private_cost'] / 1e9
-    total_costs['total_government_cost'] = total_costs['total_government_cost'] / 1e9
-    total_costs['total_financial_cost'] = total_costs['total_financial_cost'] / 1e9
+    decile_costs['total_private_cost'] = decile_costs['total_private_cost'] / 1e9
+    decile_costs['total_government_cost'] = decile_costs['total_government_cost'] / 1e9
+    decile_costs['total_financial_cost'] = decile_costs['total_financial_cost'] / 1e9
 
-    total_costs.columns = [
-        'Scenario', 'Strategy', 'Confidence', 'Decile',
-        'Population (>10 Years)', 'Area (Km2)', 'Private Cost ($Bn)',
-        'Government Cost ($Bn)', 'Financial Cost ($Bn)'
-    ]
+    path = os.path.join(OUTPUT, 'decile_cost_estimates.csv')
+    decile_costs.to_csv(path, index=False)
 
-    return total_costs
+    return
 
 
-def processing_total_costs(regional_results):
+def processing_total_costs():
     """
 
     """
-    total_costs = regional_results[[
-        'scenario', 'strategy', 'confidence', 'population',
+    path = os.path.join(OUTPUT, 'user_cost_estimates.csv')
+    data = pd.read_csv(path)
+
+    total_costs = data[[
+        'scenario', 'strategy', 'confidence', 'input_cost', 'population',
         'area_km2', 'total_private_cost',
         'total_government_cost', 'total_financial_cost'
     ]]
 
     total_costs = total_costs.groupby([
-        'scenario', 'strategy', 'confidence'], as_index=True).sum().reset_index()
+        'scenario', 'strategy', 'confidence', 'input_cost'], as_index=True).sum().reset_index()
 
     total_costs['total_private_cost'] = total_costs['total_private_cost'] / 1e9
     total_costs['total_government_cost'] = total_costs['total_government_cost'] / 1e9
     total_costs['total_financial_cost'] = total_costs['total_financial_cost'] / 1e9
 
-    total_costs.columns = [
-        'Scenario', 'Strategy', 'Confidence',
-        'Population (>10 Years)', 'Area (Km2)', 'Private Cost ($Bn)',
-        'Government Cost ($Bn)', 'Financial Cost ($Bn)'
-    ]
+    path = os.path.join(OUTPUT, 'total_cost_estimates.csv')
+    total_costs.to_csv(path, index=False)
 
     return total_costs
 
@@ -377,83 +360,8 @@ if __name__ == '__main__':
     if not os.path.exists(OUTPUT):
         os.makedirs(OUTPUT)
 
-    # capacities = [
-    #     10,
-    #     2
-    # ]
-
-    # for capacity in capacities:
-
-    # print('Working on {} Mbps per user'.format(capacity))
-
-    #Loading PODIS cost estimate data
-    filename = 'regional_mno_cost_results_technology_options.csv'
-    path = os.path.join(RESULTS, filename)
-    data = pd.read_csv(path)
-
-    #Processing PODIS data
-    data = process_costs()
-
-    #     #Writing regional per user cost data to .csv
-    #     filename = 'regional_per_user_cost_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     data.to_csv(path, index=False)
-
-    #     #Summarizing data
-    #     costs = summarize_data(data, capacity)
-
-    #     #Writing summarized data to .csv
-    #     filename = 'per_user_cost_by_pop_density_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     costs.to_csv(path, index=False)
-
-    #     #Loading regional data by pop density geotype
-    #     filename = 'all_regional_data.csv'
-    #     path = os.path.join(DATA_INTERMEDIATE, filename)
-    #     regional_data = pd.read_csv(path)
-
-    #     #Processing all regional data
-    #     regional_data = process_all_regional_data(regional_data)
-
-    #     #Combining regional data with costs
-    #     regional_data = get_costs(regional_data, costs)
-
-    #     #Exporting results
-    #     regional_data = pd.DataFrame(regional_data)
-    #     filename = 'regional_cost_estimates_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     regional_data.to_csv(path, index=False)
-
-    #     #Processing results data
-    #     national_costs = processing_national_costs(regional_data, capacity)
-
-    #     #Exporting national results
-    #     filename = 'national_cost_estimates_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     national_costs.to_csv(path, index=False)
-
-    #     #Processing total cost data
-    #     decile_costs = processing_decile_costs(regional_data)
-
-    #     #Exporting national results
-    #     filename = 'decile_cost_estimates_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     decile_costs.to_csv(path, index=False)
-
-    #     #Processing total cost data
-    #     total_costs = processing_total_costs(regional_data)
-
-    #     #Exporting national results
-    #     filename = 'total_cost_estimates_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, filename)
-    #     total_costs.to_csv(path, index=False)
-
-    #     #Estimate the percentage savings by technology
-    #     percentages = process_total_cost_data(total_costs, capacity)
-
-    #     #Writing regional per user cost data to .csv
-    #     filename = 'percentages_total_cost_{}.csv'.format(capacity)
-    #     path = os.path.join(OUTPUT, '..', 'percentages', filename)
-    #     percentages.to_csv(path, index=False)
-
-    # print('-- Processing completed')
+    process_costs()
+    process_all_regional_data()
+    processing_national_costs()
+    processing_decile_costs()
+    processing_total_costs()
